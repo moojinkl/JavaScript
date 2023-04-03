cc76ee96e909fee328a8c9d58cdf8a9fd8a3baa3
yeji.choi <moojinkl@gmail.com>
2023-04-03 16:12:00 +0900

    연산자 | 0403

---
+++ 230403/017_null.js
@@ -0,0 +1,27 @@
+let value1; // undefined
+
+// 존재하지만 값이나 자료형이 존재하지 않는 foo
+var value2 = null;
+console.log(value2); // null
+
+// 참고 삼아서만 알아두세요.
+
+// let 반점수 = [10, 20, 30, null, 40, 50]
+// let 반평균 = 0
+// for (const i of 반점수) {
+//     console.log(i)
+//     반평균 += i
+// }
+
+// 반평균
+
+//////////////////////////
+
+// let 반점수 = [10, 20, 30, undefined, 40, 50];
+// let 반평균 = 0;
+// for (const i of 반점수) {
+//   console.log(i);
+//   반평균 += i;
+// }
+
+// 반평균; // 비어있는 값은 null로 표기
