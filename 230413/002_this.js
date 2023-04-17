c35c270bc587e64205c54148178972d9de93242e
yeji.choi <moojinkl@gmail.com>
2023-04-17 09:36:35 +0900

    this, date 보충 | 0417

--- 230413/002_this.js
+++ 230413/002_this.js
@@ -169,7 +169,43 @@ jombi.damaged1[0](); // Beam
 jombi.damaged2[1](); // Knife
 // jombi.hp;
 
-///
+///////////////////////////////
+
+function attackBeam() {
+  // 레이저 공격
+  this.hp -= 20;
+}
+
+function attackKnife() {
+  // 칼공격
+  if (this.name === "thanos") {
+    this.hp -= 1;
+    return;
+  }
+  this.hp -= 5;
+}
+
+let jombi = {
+  name: "jombi",
+  damaged1: attackBeam,
+  damaged2: attackKnife,
+  hp: 10000,
+  power: 2,
+};
+
+let thanos = {
+  name: "thanos",
+  damaged1: attackBeam,
+  damaged2: attackKnife,
+  hp: 10000,
+  power: 100,
+};
+
+jombi.damaged1(); // Beam
+jombi.damaged2(); // Knife
+jombi.hp;
+
+//////////////////////////////
 
 // this 사용 예시
 let 호텔 = [
--- 230417/001_date.js
+++ 230417/001_date.js
@@ -43,16 +43,18 @@ d.getFullYear();
 // new Date(2023, 6, 30)
 new Date(2023, 5, 30); // 6월을 표시하기 위해서는 -1을 해야 합니다. 월은 0부터 시작합니다.
 
-new Date(2023, 5, 30, 18);
+new Date(2023, 5, 30, 18); // Fri Jun 30 2023 18:00:00 GMT+0900 (한국 표준시)
 
 // 다음처럼 표현하면 가독성이 훨씬 좋다. 여기에서 문제가 발생! 왜 1이(2월) 아닌가??
 new Date("2023/1/20/10:00:00");
 
 today = new Date(); // today의 지정 로캘은 KST다.
 
-/////////////////// 4월 13일 진도 /////////////////////
-
-//UTC와 today의 지정 로캘 KST와의 차이는 -9시간이다.
+// UTC와 today의 지정 로캘(locale) KST와의 차이는 -9시간이다.
+// 쉬운 말로 협정 세계시(Universal Time Coordinated)와 내 컴퓨터 시간 차가 -9시간
+// https://ko.wikipedia.org/wiki/%ED%98%91%EC%A0%95_%EC%84%B8%EA%B3%84%EC%8B%9C
+// locale 을 활용하면 언어권에 맞게 입력과 출력을 수정하지 않고도 사용하는 언어권에 맞는 날짜를 처리할 수 있습니다.
+//UTC와 현재 로케일(호스트 시스템, today의 지정 로캘 KST(Korea Standard Time))의 차이는 -9시간이다.
 today.getTimezoneOffset() / 60;
 
 today.toString(); // -> Fri Jul 24 2020 12:30:00 GMT+0900 (대한민국 표준시)
@@ -61,6 +63,12 @@ today.toTimeString(); // -> 12:30:00 GMT+0900 (대한민국 표준시)
 today = new Date("2023/1/20/10:00:00");
 today.toString();
 today.toISOString();
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
+// 국제표준시 기준 형식으로 표현
+today.toISOString();
+today.toISOString().slice(0, 10);
+today.toISOString().slice(0, 10).replace(/-/g, "");
 today.toISOString().slice(0, 10);
 today.toISOString().slice(0, 10).replace(/-/g, "");
 
@@ -89,3 +97,16 @@ let hour = today.getHours();
 let minute = today.getMinutes();
 let second = today.getSeconds();
 const ampm = hour >= 12 ? "PM" : "AM";
+
+// 날짜의 차를 구하는 코드
+// getTime은 1970 년 1 월 1 일 00:00:00 UTC와 주어진 날짜 사이의 경과 시간 (밀리 초)을 나타내는 숫자
+function getDateDiff(d1, d2) {
+  const date1 = new Date(d1);
+  const date2 = new Date(d2);
+  const diffDate = date1.getTime() - date2.getTime();
+  // 일 == 밀리세컨 * 초 * 분 * 시
+  return Math.abs(diffDate / (1000 * 60 * 60 * 24));
+}
+
+getDateDiff("2023-02-27", "2023-06-30");
+getDateDiff("2023-04-17", "2023-06-30");
